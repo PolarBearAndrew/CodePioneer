69a820a11b9f52043694f87526ce00b6a93900b2
Andrew <chenpoanandrew@gmail.com>
2015-07-08 11:16:25 +0800

    rename test.js to user.js

--- server/model/user.js
+++ server/model/user.js
@@ -1,4 +1,4 @@
-var mongoose = require('mongoose').connect('mongodb://127.0.0.1:27017/CodePioneerTestDB_User'),
+var mongoose = require('mongoose').connect('mongodb://127.0.0.1:27017/CP_user'),
     db = mongoose.connection;
 
 /*
