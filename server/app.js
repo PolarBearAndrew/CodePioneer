ca2d7b8eee9a25b4f1aa6f1e3b52170f7504640b
Andrew <chenpoanandrew@gmail.com>
2015-07-08 12:21:21 +0800

    remove test

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var test = require('./routes/test.js');
 
 var app = express();
 
@@ -25,8 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api/users', users);
-app.use('/API/test', test);
-app.use('/API/user', test);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
