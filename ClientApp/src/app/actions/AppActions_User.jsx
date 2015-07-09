101bcc445fecfb935251a8f9655515d846cf7e16
Andrew <chenpoanandrew@gmail.com>
2015-07-09 08:46:05 +0800

    add flux structure, not working now

---
+++ ClientApp/src/app/actions/AppActions_User.jsx
@@ -0,0 +1,9 @@
+let AppActions_User = {
+
+	load(){
+		console.log('load data');;
+	}
+
+}
+
+module.exports = AppActions_User;
---
+++ ClientApp/src/app/stores/UserStore.js
@@ -0,0 +1,71 @@
+/**
+ * TodoStore
+ */
+
+//========================================================================
+//
+// IMPORT
+
+let AppDispatcher = require('../dispatcher/AppDispatcher');
+let AppConstants = require('../constants/AppConstants');
+let actions = require('../actions/AppActions_User.jsx');
+
+let objectAssign = require('object-assign');
+let EventEmitter = require('events').EventEmitter; // 取得一個 pub/sub 廣播器
+
+//========================================================================
+//
+
+var user = {
+    name: 'AndrewChen',
+    email: 'chenpoanandrew@gmail.com',
+}
+
+var isLogin = false;
+
+/**
+ * 建立 Store class，並且繼承 EventEMitter 以擁有廣播功能
+ */
+objectAssign( Store, EventEmitter.prototype, {
+
+    /**
+     * Public API
+     * 供外界取得 store 內部資料
+     */
+    getUer: function(){
+        return user;
+    },
+
+    //
+    noop: function(){}
+});
+
+//========================================================================
+//
+// event handlers
+
+Store.dispatchToken = AppDispatcher.register( function eventHandlers(evt){
+
+    // evt .action 就是 view 當時廣播出來的整包物件
+    // 它內含 actionType
+    var action = evt.action;
+
+    switch (action.actionType) {
+
+        /**
+         *
+         */
+        case AppConstants.USER_LOGIN:
+
+            isLogin = true;
+
+            Store.emit( AppConstants.CHANGE_EVENT );
+
+            break;
+
+
+        default:
+    }
+})
+
+module.exports = Store;
