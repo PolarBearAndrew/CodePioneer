e16155763493d81a0944f815d47ef9ce8e25b1ea
Dobubu <rilakkuma0330k@gmail.com>
2015-07-13 13:13:12 +0800

    login.jsx

--- ClientApp/src/app/components/main.jsx
+++ ClientApp/src/app/components/main.jsx
@@ -8,16 +8,11 @@ let Colors = mui.Styles.Colors;
 let ThemeManager = new mui.Styles.ThemeManager();
 
 //mui 元件
-let Dialog = mui.Dialog;
-let AppBar = mui.AppBar;
-let TimePicker = mui.TimePicker;
-let RaisedButton = mui.RaisedButton;
-let LeftNav = mui.LeftNav;
-let MenuItem = mui.MenuItem;
-
+let TextField=mui.TextField;
+let RaisedButton=mui.RaisedButton;
 
 //react 自製元件
-let MyCard = require('./myCard.jsx');
+
 
 //flux 資料相關
 let Actions = require('../actions/AppActions_User.jsx');
@@ -46,92 +41,41 @@ let Main = React.createClass({
 
 	//顯示畫面的func
 	render() {
-
-		//取得使用者資料
-		let user = UserStore.getUser();
-
-		//css
+        //css
 	    let containerStyle = {
-	    	textAlign: 'center',
 	    	padding: '0px',
-	    	margin: '0px'
+            height : '300px',
+            width : '300px',
+            display: '-webkit-flex',
+            display:'flex',
+            alignItems: 'center',
+            justifyContent: 'center', 
+            flexDirection: 'column'
 	    };
-
-	    //mui dialog的按鈕設定資料
-	    let standardActions = [
-	    	{ text: 'sure' },
-	    	{ text: 'submit' }
-	    ];
-
-	    //mui left menu 的設定資料
-	    var menuItems = [
-		  { route: 'get-started', text: 'Get Started' },
-		  { route: 'customization', text: 'Customization' },
-		  { route: 'components', text: 'Components' },
-		  { type: MenuItem.Types.SUBHEADER, text: 'Resources' },
-		  {
-		     type: MenuItem.Types.LINK,
-		     payload: 'https://github.com/PolarBearAndrew/CodePioneer',
-		     text: 'GitHub'
-		  },
-		  {
-		     text: 'Disabled',
-		     disabled: true
-		  },
-		  {
-		     type: MenuItem.Types.LINK,
-		     payload: 'https://www.google.com',
-		     text: 'Disabled Link',
-		     disabled: true
-		  },
-		];
-
-
+        
 	    return (
 	    	<div style={containerStyle}>
+                <TextField 
+                        hintText="input your Account"
+                        multiLine={true} />
 
-	    		<AppBar title='CodePioneer'
-	    			iconClassNameRight="muidocs-icon-navigation-expand-more"
-	    			onLeftIconButtonTouchTap={this._togleLeftNav} />
-
-		        <h2>material-ui demo</h2>
-
-		        <MyCard id={1} />
-
-		        <MyCard id={2} />
-
-		        <MyCard id={3} />
-
-				<TimePicker
-				  format="ampm"
-				  hintText="12hr Format" />
-
-		        <RaisedButton label="Show Msg" primary={true} onTouchTap={this._handleTouchTap} />
-
-		        <LeftNav docked={false} menuItems={menuItems} ref='leftNav'/>
-
-		        <Dialog
-			        title="HI PANDA"
-			        actions={standardActions}
-			        ref="myDialog">
-			        You can type any thing you want to say at here
-		        </Dialog>
-
-
-	     	</div>
+                <TextField
+                    hintText="input your password"
+                    defaultValue="Custom Child input (e.g. password)"
+                    floatingLabelText="">
+                    <input type="password" />
+                    </TextField>
+                <br/><br/> 
+                <RaisedButton label="Login" primary={true} />   
+                <h4>Forger your password</h4>
+            </div>
 	    );
 	},
+    
+    _handleErrorInputChange(){
+        
+    },
 
-	//觸碰或點擊按鈕的事件 掛載在RaisedButton上面
-	_handleTouchTap() {
-		this.refs.myDialog.show();
-		Actions.load();
-	},
-
-	//觸碰或點擊left menu的左側icon事件
-	_togleLeftNav(){
-		this.refs.leftNav.toggle();
-	}
 
 });
 
