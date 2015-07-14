3f1dca8ea080c29f612ad7bbf15837a403a5cb64
Andrew <chenpoanandrew@gmail.com>
2015-07-14 10:34:14 +0800

    article save now

--- serverApp/routes/crawl.js
+++ serverApp/routes/crawl.js
@@ -18,27 +18,34 @@ function crawl(){
 		        //2.在json找出有價值的資料,塞到對應的欄位中
 		        //3.先想辦法塞入一筆資訊
 
+		        var article = new models.Article({
+			        title: '',
+				    url: '',
+				    author: '',
+				    describe: '',
+				    rank: '',
+				    info: ['', ''],
+			    });
+
 
 		        //for(collection跑完)
 		        //先將傳過來的資料做成資料庫物件
-			    var user = new models.User({
-			        title: result.collection[0].title,
-			        email: req.body.email,
-			        pwd: req.body.pwd
-			    });
+			    // var user = new models.User({
+			    //     title: result.collection[0].title,
+			    //     email: req.body.email,
+			    //     pwd: req.body.pwd
+			    // });
 
-			    //儲存到資料庫
-			    user.save(function(err, result) {
+			    // //儲存到資料庫
+			    article.save(function(err, result) {
 
 			        if (err) {
-			            console.log('[TEST] create test user FAIL, err ->', err);
-			            res.json(err);
+			            console.log('[TEST] create article FAIL, err ->', err);
+			            //res.json(err);
 			        } else {
-			            res.json(result);
+			            console.log(result);
 			        }
 			    });
-
-
 		});
 	};
 }
