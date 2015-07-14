33de0b8895c93b75924e553543c0d90bb7a25402
q3856245 <q3856245@gmail.com>
2015-07-14 11:41:04 +0800

    完成 爬蟲文章一筆資料API

--- serverApp/routes/crawl.js
+++ serverApp/routes/crawl.js
@@ -10,38 +10,25 @@ function crawl(){
 		// }, (1000) ); //5 min
 		//
 		request("https://www.kimonolabs.com/api/2ydhrtbc?apikey=YTFXE6bo643qztfqtgMJxbTghxkihceB",
-		    function(err, res, result) {
-		        //console.log(body);
-
-		        //將result中的資料 以Aricle的格式塞到mongodb裡面
-		        //1.把user換成article
-		        //2.在json找出有價值的資料,塞到對應的欄位中
-		        //3.先想辦法塞入一筆資訊
-
+		    function(err, res, data) {
+            
+                data = JSON.parse(data);
+            
 		        var article = new models.Article({
-			        title: '',
-				    url: '',
-				    author: '',
-				    describe: '',
-				    rank: '',
-				    info: ['', ''],
+			        title: data.results.collection1[1].title.text,
+				    url: data.results.collection1[1].title.href,
+				    author: data.results.collection1[1].users.text,
+				    describe: data.results.collection1[1].comments.href,
+                    //rank命名錯誤　命危pank
+				    rank: data.results.collection1[1].pank,
+				    info: [data.results.collection1[1].title.text, data.results.collection1[1].title.href]
 			    });
-
-
-		        //for(collection跑完)
-		        //先將傳過來的資料做成資料庫物件
-			    // var user = new models.User({
-			    //     title: result.collection[0].title,
-			    //     email: req.body.email,
-			    //     pwd: req.body.pwd
-			    // });
 
 			    // //儲存到資料庫
 			    article.save(function(err, result) {
 
 			        if (err) {
 			            console.log('[TEST] create article FAIL, err ->', err);
-			            //res.json(err);
 			        } else {
 			            console.log(result);
 			        }
