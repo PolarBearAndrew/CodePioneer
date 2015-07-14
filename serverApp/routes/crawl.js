b97077af29a36465627155d42906a55bdc47b0fa
Andrew <chenpoanandrew@gmail.com>
2015-07-14 11:44:21 +0800

    add forEach func for crawl

--- serverApp/routes/crawl.js
+++ serverApp/routes/crawl.js
@@ -11,28 +11,32 @@ function crawl(){
 		//
 		request("https://www.kimonolabs.com/api/2ydhrtbc?apikey=YTFXE6bo643qztfqtgMJxbTghxkihceB",
 		    function(err, res, data) {
-            
+
                 data = JSON.parse(data);
-            
-		        var article = new models.Article({
-			        title: data.results.collection1[1].title.text,
-				    url: data.results.collection1[1].title.href,
-				    author: data.results.collection1[1].users.text,
-				    describe: data.results.collection1[1].comments.href,
-                    //rank命名錯誤　命危pank
-				    rank: data.results.collection1[1].pank,
-				    info: [data.results.collection1[1].title.text, data.results.collection1[1].title.href]
-			    });
 
-			    // //儲存到資料庫
-			    article.save(function(err, result) {
+                data.results.collection1.forEach(function( item ){
 
-			        if (err) {
-			            console.log('[TEST] create article FAIL, err ->', err);
-			        } else {
-			            console.log(result);
-			        }
-			    });
+			        var article = new models.Article({
+				        title: item.title.text,
+					    url: item.title.href,
+					    author: item.users.text,
+					    describe: item.comments.href,
+	                    //rank命名錯誤　命危pank
+					    rank: item.pank,
+					    info: [
+					    	item.title.text,
+					    	item.title.href
+					    ]
+				    });
+
+				    //儲存到資料庫
+				    article.save(function(err, result) {
+
+				        if (err) console.log('[TEST] create article FAIL, err ->', err);
+				    });
+                });
+
+
 		});
 	};
 }
