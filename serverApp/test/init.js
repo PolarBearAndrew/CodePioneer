25861fd4bf53dab32d8fdbd0c3d84ba9e451fa02
Andrew <chenpoanandrew@gmail.com>
2015-07-16 00:11:58 +0800

    restart api unit test

--- serverApp/test/init.js
+++ serverApp/test/init.js
@@ -1,10 +1,12 @@
+// 加上這句就會自動載入 es6 檔案並即時編譯為 es5
 require('babel/register');
+require('./api-users.js');
 
-process.env.NODE_ENV = 'test';
+// process.env.NODE_ENV = 'test';
 
-var app = require('../app.js')
-  , chai = require('chai')
-  , request = require('supertest');
+// var app = require('../app.js')
+//   , chai = require('chai')
+//   , request = require('supertest');
 
-global.should = chai.should();
-global.request = request(app);
+// global.should = chai.should();
+// global.request = request(app);
