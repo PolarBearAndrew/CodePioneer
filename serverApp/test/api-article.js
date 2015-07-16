e199f70768af8a652b41981910c0a93a056ed550
Andrew <chenpoanandrew@gmail.com>
2015-07-16 11:25:03 +0800

    [POST] 新增文章

---
+++ serverApp/test/api-article.js
@@ -0,0 +1,60 @@
+var request = require('request');
+
+//init data
+var initData = {
+    title: 'New JavaScript in 2015 !!',
+    url: 'https://www.google.com.tw',
+    author: 'AndrewChen',
+    from: 'FaceBook',
+    describe: 'this is the world future...',
+    info: ['200 comments', '999 likes']
+};
+
+var aid = null;
+
+var Article = require('../models/article.js');
+
+describe('[ API unit test - articles ]', function() {
+
+    before(function() {
+
+        return Article.remove({}, (err, result) => {
+
+            //init data
+            var article = new Article(initData);
+            article.save((err, result) => {
+                aid = result._id.toString();
+            });
+        });
+    });
+
+    describe('正常操作測試', () => {
+
+        it('[POST] 新增文章', ( done ) => {
+
+            request({
+                url: 'http://localhost:8080/api/articles/',
+                method: 'POST',
+                form: initData
+            }, (err, res, data) => {
+
+                data = JSON.parse( data );
+
+                // //test api exist
+                res.statusCode.should.equal(200);
+
+                // //test data
+                data.should.have.property('_id');
+                data.should.have.property('title', initData.title);
+
+                return done();
+            });
+        });
+    });
+
+    after(function(){
+
+        // 任何需要在測試後刪除的資料
+        //console.log('after');
+    });
+});
