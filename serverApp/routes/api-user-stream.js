487738ae8ab3488129974fbe08203718aa26b822
q3856245 <q3856245@gmail.com>
2015-07-23 14:16:25 +0800

    增加註解

--- serverApp/routes/api-user-stream.js
+++ serverApp/routes/api-user-stream.js
@@ -13,6 +13,58 @@ let postMan = require('../feature/mail.js');
 let checkPorperty = require('../feature/checkProperty.js');
 let check = checkPorperty.check;
 
+/*
+ * [GET] 查詢使用者(10)
+ * request :
+ * respone : db result
+ */
+router.get('/news/:count', (req, res, next) => {
+    
+});
 
+/*
+ * [GET] 接續查詢使用者(10)
+ * request :
+ * respone : db result
+ */
+router.get('/moreNews/:count', (req, res, next) => {
+    
+});
+
+/*
+ * [GET] 查詢喜愛使用者(10)
+ * request :
+ * respone : db result
+ */
+router.get('/like/:count', (req, res, next) => {
+    
+});
+
+/*
+ * [GET] 接續查詢喜愛使用者(10)
+ * request :
+ * respone : db result
+ */
+router.get('/moreLike/:count', (req, res, next) => {
+    
+});
+
+/*
+ * [GET] 查詢追蹤使用者(10)
+ * request :
+ * respone : db result
+ */
+router.get('/follow/:count', (req, res, next) => {
+    
+});
+
+/*
+ * [GET] 接續查詢追蹤使用者(10)
+ * request :
+ * respone : db result
+ */
+router.get('/moreFollow/:count', (req, res, next) => {
+    
+});
 
 module.exports = router;
