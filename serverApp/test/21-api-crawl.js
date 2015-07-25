f52336c30ef7a264db97065178b2867a9d71a7f1
Andrew <chenpoanandrew@gmail.com>
2015-07-25 08:55:20 +0800

    fix file name

--- serverApp/test/21-api-crawl.js
+++ serverApp/test/21-api-crawl.js
@@ -21,7 +21,7 @@ let config = require('../config.js');
 //models
 let Article = require('../models/article.js');
 
-describe('[ API unit test - articles-assignClass ]', function() {
+describe('[ DB data check - crawl ]', function() {
 
     before( () => {
 
