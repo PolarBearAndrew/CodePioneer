2af041ede57addf0e5ab7e6ffdffb1ab65da8b0e
q3856245 <q3856245@gmail.com>
2015-07-29 12:27:20 +0800

    完成 [GET] 接續查詢使用者(10) 刪掉[GET] 接續查詢使用者(10) [GET] 接續查詢使用者(10) 未完成 查詢喜愛使用者

--- serverApp/routes/api-user-stream.js
+++ serverApp/routes/api-user-stream.js
@@ -55,7 +55,7 @@ router.get('/stream', (req, res, next) => {
     }
 
     let finalIndex = req.query.finalIndex;
-    let count = ( parseInt(finalIndex) + 10 );
+    let count = ( parseInt(finalIndex) + 10 );//
 
     //db operation
      User.find()
@@ -127,7 +127,7 @@ router.get('/like/stream', (req, res, next) => {
     }
     let mis = check( req.query, ['finalIndex', 'lastestTime'] );
     if(!mis.check){
-        debug('[GET] 接續查詢喜愛使用者(10) miss data ->', miss.missData);
+        debug('[GET] 接續查詢喜愛使用者(10) miss data ->', mis.missData);
         return next(err);
     }
 
@@ -151,82 +151,6 @@ router.get('/like/stream', (req, res, next) => {
                 debug('[GET] 接續查詢喜愛使用者(10) fail ->', err);
                 return next(err);
             });
-});
-
-/*
- * [GET] 查詢追蹤使用者(10)
- * request : body.uid
- * respone : db result
- */
-router.get('/follow', (req, res, next) => {
-    debug('[GET] 查詢追蹤 req.body ->', req.body );
-
-    //check
-    let miss = check( req.body, ['uid'] );
-    if(!miss.check){
-        debug('[GET] 查詢追蹤 miss data ->', miss.missData);
-        return next(err);
-    }
-
-    //destination info
-    var info = { _id: req.body.uid };
-
-    //db operation
-    User.find(info)
-        .limit(10)
-        .execAsync()
-        .then( (result) => {
-            debug('[GET] 查詢追蹤 success ->', result);
-            res.json(result);
-            return;
-        })
-        .catch( (err) => {
-            debug('[GET] 查詢追蹤 fail ->', err);
-            return next(err);
-        });
-});
-
-/*
- * [GET] 接續查詢追蹤使用者(10)
- * request : body.uid, query.finalIndex, query.lastestTime
- * respone : db result
- */
-router.get('/follow/stream', (req, res, next) => {
-
-    debug('[GET] 查詢追蹤 req.body ->', req.body );
-
-    //check
-    let miss = check( req.body, ['uid'] );
-    if(!miss.check){
-        debug('[GET] 查詢追蹤 miss data ->', miss.missData);
-        return next(err);
-    }
-    let mis = check( req.query, ['finalIndex', 'lastestTime'] );
-    if(!mis.check){
-        debug('[GET] 接續查詢喜愛使用者(10) miss data ->', miss.missData);
-        return next(err);
-    }
-
-    let finalIndex = req.query.finalIndex;
-    let count = ( parseInt(finalIndex) + 10 );
-
-    //destination info
-    var info = { _id: req.body.uid };
-
-    //db operation
-    User.find(count)
-        .limit(10)
-        .where('time <= ' + req.query.lastestTime)
-        .execAsync()
-        .then( (result) => {
-            debug('[GET] 查詢追蹤 success ->', result);
-            res.json(result);
-            return;
-        })
-        .catch( (err) => {
-            debug('[GET] 查詢追蹤 fail ->', err);
-            return next(err);
-        });
 });
 
 module.exports = router;
--- serverApp/test/06-api-user-stream.js
+++ serverApp/test/06-api-user-stream.js
@@ -8,11 +8,21 @@ var User = require('../models/user.js');
 
 var tmpAidForLike = null;
 var lastestTime = null;
+var uid = null;
 
 describe('[ (06) API unit test - user-stream ]', () => {
 
-    before( (done) => {
-        return done();
+    before( () => {
+
+        return User.remove({ author: 'AndrewChen' }, (err, result) => {
+
+            //init data
+            var user = new Article(initData);
+            user.save((err, result) => {
+                uid = result._id;
+                lastestTime = result.lastestTime;
+            });
+        });
     });
 
     describe('正常操作測試', () => {
@@ -38,73 +48,54 @@ describe('[ (06) API unit test - user-stream ]', () => {
 //                return done();
 //            });
 //        });
+//
+//        it('[GET] 接續查詢使用者(10)', ( done ) => {
+//            let finalIndex = 10;
+//
+//            request({
+//                url: queryString('http://localhost:8080/api/users/stream/stream', { finalIndex, lastestTime } ),
+//                method: 'GET',
+//                json: true,
+//            }, (err, res, data) => {
+//
+//                //test api exist
+//                should.exist(data);
+//                should.not.exist(err);
+//                res.statusCode.should.equal(200);
+//
+//                //test data
+//                data.should.with.lengthOf(10);
+//
+//                return done();
+//            });
+//        });
 
-        it('[GET] 接續查詢使用者(10)', ( done ) => {
-            let finalIndex = 4;
-
+        it('[GET] 查詢喜愛使用者(10)', ( done ) => {
+            
+            
             request({
-                url: queryString('http://localhost:8080/api/users/stream/stream', { finalIndex, lastestTime } ),
+                url: queryString('http://localhost:8080/api/users/stream/like', {  } ),
                 method: 'GET',
                 json: true,
             }, (err, res, data) => {
-
+                
                 //test api exist
                 should.exist(data);
                 should.not.exist(err);
                 res.statusCode.should.equal(200);
 
                 //test data
-                data.should.with.lengthOf(4);
+                data.should.with.lengthOf(10);
+
+                tmpAidForLike = data;
 
                 return done();
             });
         });
 
-        it('[GET] 查詢喜愛使用者(10)', ( done ) => {
-//            request({
-//                url: queryString('http://localhost:8080/api/users/stream/like'),
-//                method: 'GET',
-//                json: true,
-//            }, (err, res, data) => {
-//                
-//            });
-            //加快測試錯誤用的資料
-            let data = null;
-            should.exist(data);
-            return done();
-        });
-
         it('[GET] 接續查詢喜愛使用者(10)', ( done ) => {
 //            request({
 //                url: queryString('http://localhost:8080/api/users/stream/like/stream', { finalIndex, lastestTime } ),
-//                method: 'GET',
-//                json: true,
-//            }, (err, res, data) => {
-//                
-//            });
-            //加快測試錯誤用的資料
-            let data = null;
-            should.exist(data);
-            return done();
-        });
-
-        it('[GET] 查詢追蹤使用者(10)', ( done ) => {
-//           request({
-//                url: queryString('http://localhost:8080/api/users/stream/follow'),
-//                method: 'GET',
-//                json: true,
-//            }, (err, res, data) => {
-//                
-//            });
-            //加快測試錯誤用的資料
-            let data = null;
-            should.exist(data);
-            return done();
-        });
-        
-        it('[GET] 接續查詢追蹤使用者(10)', ( done ) => {
-//            request({
-//                url: queryString('http://localhost:8080/api/users/stream/follow/stream', { finalIndex, lastestTime } ),
 //                method: 'GET',
 //                json: true,
 //            }, (err, res, data) => {
