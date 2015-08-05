534a4a9caf859b11337d59c3d7a6dc1a41162cbb
Andrew <chenpoanandrew@gmail.com>
2015-08-05 22:18:00 +0800

    calculate done

--- serverApp/feature/calculate.js
+++ serverApp/feature/calculate.js
@@ -1,27 +1,21 @@
 
-
-//rank增減演算
-//var: 原始rank值
-let rank = ( origin, diff) => {
+//rank increase
+let rank = ( origin, diff ) => {
 	let val = origin || 0;					// check parameters
 	val *= Math.log(diff) / Math.log(2); 	//y = log2(diff);
 	return val;
 };
 
 //f(x,y) = 16x + 30y - 240 = 0
-
-let check = ( rank, time, num) => {
-
-	let tolerant = num || 0;
-	let val = 16 * rank + 30 * time - 240 + tolerant;
-
-	if( val > 0)
-		return true;
-	else
-		return false;
+let check = ( rank, time, num ) => {
+	let tolerant = num || 0;							// check parameters
+	let val = 16 * rank + 30 * time - 240 + tolerant;	//f(x,y)
+	if( val > 0) return true;
+	else return false;
 };
 
-let weak = (rank, time) => {
+//weak every 12hour
+let weak = ( rank, time ) => {
 	let val = {
 		rank: rank * .0.97;
 		time: time - 10 / time
@@ -29,4 +23,4 @@ let weak = (rank, time) => {
 	return val;
 }
 
-module.exports = { rank, time, check, weak };
+module.exports = { rank, check, weak };
