569f32b3f18e6fd1de14a0ce449b9d973ae91f90
q3856245 <q3856245@gmail.com>
2015-09-17 10:40:05 +0800

    重新爬過資料(hacknes),其餘需重新爬蟲

--- serverApp/feature/crawl.js
+++ serverApp/feature/crawl.js
@@ -24,7 +24,7 @@ function crawl(){
 		 * Hacker News
 		 */
 		let getHackerNews = () => {
-			request("https://www.kimonolabs.com/api/2ydhrtbc?apikey=YTFXE6bo643qztfqtgMJxbTghxkihceB",
+			request("https://www.kimonolabs.com/api/9fqa0a2s?apikey=7yjRQtS3sJ9oRobTONiJDzT1rm4Qgknt",
 		   		(err, res, data) => {
 
 	                data = JSON.parse(data);
@@ -66,193 +66,193 @@ function crawl(){
 	            	});
             	});
 		}
-
-		/*
-		 * Github top 10
-		 */
-        let getGithun10 = () => {
-        	request("https://www.kimonolabs.com/api/bsujce7y?apikey=7yjRQtS3sJ9oRobTONiJDzT1rm4Qgknt",
- 		    function(err, res, data) {
-
-                 data = JSON.parse(data);
-
-                 debug('[crawl] Github top 10 get data ->', data.results.github_top10);
-
-                 data.results.github_top10.forEach(function( item ){
-
- 			        var article = new Article({
-                         //title
- 				        title: item.title.text,
-                         //文章的url
- 					    url: item.title.href,
-                         //作者
- 					    author: item.author,
-                         //來源
-                        from: config.crawlName.github10,
-                         //描述
- 					    describe: item.describe,
-                         //一些小資訊
- 					    info: [
-                             //發布的日期
-                             item.updated,
-                             //收藏的人數
-                             item.follow_branch[0].text,
-                             //收藏連接網址
-                             item.follow_branch[0].href,
-                             //branch的人數
-                             item.branch.text,
-                             //branch連接網址
-                             item.branch.href
- 					    ]
- 				    });
-
- 				    //db operation
- 				     article.saveAsync()
-				    		.then( (result) => {
-				        		debug('[crawl] Github top 10 success ->', result);
-				    		})
-				    		.catch( (err) => {
-				    			debug('[crawl] Github top 10 fail ->', err);
-				    		});
-                 });
-			});
-		}
-
-        /*
-		 * iThome Technology
-		 */
-        let getIThomeTech = () => {
-        	request("https://www.kimonolabs.com/api/d73vr5sq?apikey=7yjRQtS3sJ9oRobTONiJDzT1rm4Qgknt",
- 		    function(err, res, data) {
-
-                 data = JSON.parse(data);
-
-				debug('[crawl] iThome Technology get data ->', data.results.ithome);
-
-                 data.results.ithome_tech.forEach(function( item ){
-
- 			        var article = new Article({
-                         //title
- 				        title: item.title.text,
-                         //文章的url
- 					    url: item.title.href,
-                        //作者
- 					    author: null,
-                         //來源
-                        from: config.crawlName.iThomeTech,
-                         //描述
- 					    describe: item.describe,
-                         //一些小資訊
- 					    info: [
-                             //發布的日期
-                             item.updated
- 					    ]
- 				    });
-
- 				    //db operation
- 				     article.saveAsync()
-				    		.then( (result) => {
-				        		debug('[crawl] iThome Technology success ->', result);
-				    		})
-				    		.catch( (err) => {
-				    			debug('[crawl] iThome Technology fail ->', err);
-				    		});
-                });
-			});
-        }
-
-        /*
-		 * iThome News
-         * updated 1 hours
-		 */
-        let getIThomeNews = () => {
-            request("https://www.kimonolabs.com/api/c9njd4bo?apikey=7yjRQtS3sJ9oRobTONiJDzT1rm4Qgknt",
- 		    function(err, res, data) {
-
-                data = JSON.parse(data);
-
-                debug('[crawl] iThome News get data ->', data.results.ithome_news);
-
-                data.results.ithome_news.forEach(function( item ){
-
- 			        var article = new Article({
-                         //title
- 				        title: item.title.text,
-                         //文章的url
- 					    url: item.title.href,
-                        //作者
- 					    author: null,
-                         //來源
-                        from: config.crawlName.iThomeNews,
-                         //描述
- 					    describe: item.describe,
-                         //一些小資訊
- 					    info: [
-                             //發布的日期
-                             item.updated
- 					    ]
- 				    });
-
- 				    //db operation
- 				     article.saveAsync()
-				    		.then( (result) => {
-				        		debug('[crawl] iThome News success ->', result);
-				    		})
-				    		.catch( (err) => {
-				    			debug('[crawl] iThome News fail ->', err);
-				    		});
-                 });
-		 });
-        }
-        
-        /*
-		 * Github Tranding
-		 */
-        let getGitTrand = () => {
-        	request("https://www.kimonolabs.com/api/2uale15w?apikey=7yjRQtS3sJ9oRobTONiJDzT1rm4Qgknt",
- 		    function(err, res, data) {
-
-                 data = JSON.parse(data);
-
-                 debug('[crawl] Github Tranding get data ->', data.results.Github_Tranding);
-
-                 data.results.Github_Tranding.forEach(function( item ){
-
- 			        var article = new Article({
-                         //title
- 				        title: item.title.text,
-                         //文章的url
- 					    url: item.title.href,
-                         //作者
- 					    author: null,
-                         //來源
-                        from: config.crawlName.githubTranding,
-                         //描述
- 					    describe: item.describe.text,
-                         //一些小資訊
- 					    info: [
-                            //更新以及建置人
-                            item.describe2.Text,
-                            //更新以及建置人連接網址
-                            item.describe2.href,
-                            //星星
-                            item.star.text,
-                            //星星連結
-                            item.star.href,
- 					    ]
- 				    });
-
- 				    //db operation
- 				     article.saveAsync()
-				    		.then( (result) => {
-				        		debug('[crawl] Github Tranding success ->', result);
-				    		})
-				    		.catch( (err) => {
-				    			debug('[crawl] Github Tranding fail ->', err);
-				    		});
-                 });
-			});
-		}
+//      爬蟲資料有問題,需重新再爬,換成新的API
+//		/*
+//		 * Github top 10
+//		 */
+//        let getGithun10 = () => {
+//        	request("https://www.kimonolabs.com/api/bsujce7y?apikey=7yjRQtS3sJ9oRobTONiJDzT1rm4Qgknt",
+// 		    function(err, res, data) {
+//
+//                 data = JSON.parse(data);
+//
+//                 debug('[crawl] Github top 10 get data ->', data.results.github_top10);
+//
+//                 data.results.github_top10.forEach(function( item ){
+//
+// 			        var article = new Article({
+//                         //title
+// 				        title: item.title.text,
+//                         //文章的url
+// 					    url: item.title.href,
+//                         //作者
+// 					    author: item.author,
+//                         //來源
+//                        from: config.crawlName.github10,
+//                         //描述
+// 					    describe: item.describe,
+//                         //一些小資訊
+// 					    info: [
+//                             //發布的日期
+//                             item.updated,
+//                             //收藏的人數
+//                             item.follow_branch[0].text,
+//                             //收藏連接網址
+//                             item.follow_branch[0].href,
+//                             //branch的人數
+//                             item.branch.text,
+//                             //branch連接網址
+//                             item.branch.href
+// 					    ]
+// 				    });
+//
+// 				    //db operation
+// 				     article.saveAsync()
+//				    		.then( (result) => {
+//				        		debug('[crawl] Github top 10 success ->', result);
+//				    		})
+//				    		.catch( (err) => {
+//				    			debug('[crawl] Github top 10 fail ->', err);
+//				    		});
+//                 });
+//			});
+//		}
+//
+//        /*
+//		 * iThome Technology
+//		 */
+//        let getIThomeTech = () => {
+//        	request("https://www.kimonolabs.com/api/d73vr5sq?apikey=7yjRQtS3sJ9oRobTONiJDzT1rm4Qgknt",
+// 		    function(err, res, data) {
+//
+//                 data = JSON.parse(data);
+//
+//				debug('[crawl] iThome Technology get data ->', data.results.ithome);
+//
+//                 data.results.ithome_tech.forEach(function( item ){
+//
+// 			        var article = new Article({
+//                         //title
+// 				        title: item.title.text,
+//                         //文章的url
+// 					    url: item.title.href,
+//                        //作者
+// 					    author: null,
+//                         //來源
+//                        from: config.crawlName.iThomeTech,
+//                         //描述
+// 					    describe: item.describe,
+//                         //一些小資訊
+// 					    info: [
+//                             //發布的日期
+//                             item.updated
+// 					    ]
+// 				    });
+//
+// 				    //db operation
+// 				     article.saveAsync()
+//				    		.then( (result) => {
+//				        		debug('[crawl] iThome Technology success ->', result);
+//				    		})
+//				    		.catch( (err) => {
+//				    			debug('[crawl] iThome Technology fail ->', err);
+//				    		});
+//                });
+//			});
+//        }
+//
+//        /*
+//		 * iThome News
+//         * updated 1 hours
+//		 */
+//        let getIThomeNews = () => {
+//            request("https://www.kimonolabs.com/api/c9njd4bo?apikey=7yjRQtS3sJ9oRobTONiJDzT1rm4Qgknt",
+// 		    function(err, res, data) {
+//
+//                data = JSON.parse(data);
+//
+//                debug('[crawl] iThome News get data ->', data.results.ithome_news);
+//
+//                data.results.ithome_news.forEach(function( item ){
+//
+// 			        var article = new Article({
+//                         //title
+// 				        title: item.title.text,
+//                         //文章的url
+// 					    url: item.title.href,
+//                        //作者
+// 					    author: null,
+//                         //來源
+//                        from: config.crawlName.iThomeNews,
+//                         //描述
+// 					    describe: item.describe,
+//                         //一些小資訊
+// 					    info: [
+//                             //發布的日期
+//                             item.updated
+// 					    ]
+// 				    });
+//
+// 				    //db operation
+// 				     article.saveAsync()
+//				    		.then( (result) => {
+//				        		debug('[crawl] iThome News success ->', result);
+//				    		})
+//				    		.catch( (err) => {
+//				    			debug('[crawl] iThome News fail ->', err);
+//				    		});
+//                 });
+//		 });
+//        }
+//        
+//        /*
+//		 * Github Tranding
+//		 */
+//        let getGitTrand = () => {
+//        	request("https://www.kimonolabs.com/api/2uale15w?apikey=7yjRQtS3sJ9oRobTONiJDzT1rm4Qgknt",
+// 		    function(err, res, data) {
+//
+//                 data = JSON.parse(data);
+//
+//                 debug('[crawl] Github Tranding get data ->', data.results.Github_Tranding);
+//
+//                 data.results.Github_Tranding.forEach(function( item ){
+//
+// 			        var article = new Article({
+//                         //title
+// 				        title: item.title.text,
+//                         //文章的url
+// 					    url: item.title.href,
+//                         //作者
+// 					    author: null,
+//                         //來源
+//                        from: config.crawlName.githubTranding,
+//                         //描述
+// 					    describe: item.describe.text,
+//                         //一些小資訊
+// 					    info: [
+//                            //更新以及建置人
+//                            item.describe2.Text,
+//                            //更新以及建置人連接網址
+//                            item.describe2.href,
+//                            //星星
+//                            item.star.text,
+//                            //星星連結
+//                            item.star.href,
+// 					    ]
+// 				    });
+//
+// 				    //db operation
+// 				     article.saveAsync()
+//				    		.then( (result) => {
+//				        		debug('[crawl] Github Tranding success ->', result);
+//				    		})
+//				    		.catch( (err) => {
+//				    			debug('[crawl] Github Tranding fail ->', err);
+//				    		});
+//                 });
+//			});
+//		}
 
          Article.removeAsync()
 				.then( ()=>{
