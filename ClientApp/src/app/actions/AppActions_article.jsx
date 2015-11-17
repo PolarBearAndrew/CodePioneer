06cd2e0660d0f307fa9771fef6710059f7531531
PolarBearAndrew <PolarBearAndrew@users.noreply.github.com>
2015-11-17 10:45:11 +0800

    update

--- ClientApp/src/app/actions/AppActions_article.jsx
+++ ClientApp/src/app/actions/AppActions_article.jsx
@@ -12,116 +12,53 @@ let dispatcher = function(type, data){
 
 let AppActions_Articles = {
 	load(){
-		fetch(address + '/news')
-			.then( response => response.json())
-			.then( result => dispatcher( ARTICLE_LOAD, result) )
-			.catch( err => dispatcher( noop, null) );
+		let url = address + '/news';
+		fetch(url)
+		.then( res => res.json())
+		.then( result => dispatcher('ARTICLE_LOAD', result))
+		.catch( err => dispatcher( 'noop', null));
 	},
 
 	loadmore( finalIndex, lastestTime ){
-		$.ajax({
-			url: address + '/stream?finalIndex=' + finalIndex + '&lastestTime=' + lastestTime,
-			type: 'GET',
-
-			success: function(result){
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.ARTICLE_LOADMORE,
-					data: result
-				});
-			},
-			error: function(err){
-
-				console.log('err', err);
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.noop,
-					data: null
-				});
-			}
-		});
+		let url = address + '/stream?finalIndex=' + finalIndex + '&lastestTime=' + lastestTime;
+		fetch(url)
+		.then( res => res.json())
+		.then( result => dispatcher('ARTICLE_LOADMORE', result))
+		.catch( err => dispatcher('noop', null));
 	},
 
 	loadLike( likeAry ){
-		$.ajax({
-			url: address + '/like',
-			type: 'GET',
-			data: { like: likeAry},
-			success: function(result){
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.ARTICLE_LOADLIKE,
-					data: result
-				});
-			},
-			error: function(err){
-
-				console.log('err', err);
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.noop,
-					data: null
-				});
-			}
-		});
+		let url = address + '/like';
+		fetch(url, {
+			query: { like: likeAry}
+		})
+		.then( res => res.json())
+		.then( result => dispatcher('ARTICLE_LOADLIKE', result))
+		.catch( err => dispatcher('noop', null));
 	},
 
 	loadhelike( uid ){
-		console.log('load like', uid);
-
-		$.ajax({
-			url: 'http://localhost:8080/api/like/',
-			type: 'GET',
-			data: { uid },
-
-			success: function(result){
-
-//				console.log('success(result)', result);
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.HELIKE_LOAD,
-					data: result
-				});
-			},
-			error: function(err){
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.noop,
-					data: null
-				});
-			}
-		});
+		let url = 'http://localhost:8080/api/like/';
+		fetch(url, {
+			query: { uid },
+		})
+		.then( res => res.json())
+		.then( result => dispatcher('HELIKE_LOAD', result))
+		.catch( err => dispatcher('noop', null));
 	},
 
 	loadTheyLiked( uid ){
-		$.ajax({
-			url: address + '/follow/like',
-			type: 'GET',
-			data: { uid },
-			success: function(result){
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.ARTICLE_LOADTHEYLIKE,
-					data: result
-				});
-			},
-			error: function(err){
-
-				console.log('err', err);
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.noop,
-					data: null
-				});
-			}
-		});
+		let url = address + '/follow/like';
+		fetch(url, {
+			query: { uid },
+		})
+		.then( res => res.josn())
+		.then( result => dispatcher('ARTICLE_LOADTHEYLIKE', result))
+		.catch( err => dispatcher('noop', null));
 	},
 
 	filter( from ){
-		AppDispatcher.handleViewAction({
-			actionType: AppConstants.ARTICLE_FILTER,
-			data: from
-		});
+		return dispatcher('ARTICLE_FILTER', from);
 	},
 };
 
