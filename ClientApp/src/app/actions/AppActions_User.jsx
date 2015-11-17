3dd2e939e3a24ec18d6423be53dfccb855329489
PolarBearAndrew <PolarBearAndrew@users.noreply.github.com>
2015-11-17 11:02:29 +0800

    update all

--- ClientApp/src/app/actions/AppActions_User.jsx
+++ ClientApp/src/app/actions/AppActions_User.jsx
@@ -5,141 +5,65 @@ let address = 'http://localhost:8080/api/users';
 let followAddress = 'http://localhost:8080/api/follow';
 let streamAaddress = 'http://localhost:8080/api/users/stream';
 
+let dispatcher = function(type, data){
+	AppDispatcher.handleViewAction({
+		actionType: AppConstants[type],
+		data: data
+	});
+};
+
 let AppActions_User = {
 
 	login( data, loginFail ){
-		$.ajax({
-			url: address + '/login',
-			type: 'POST',
-			data: data,
-
-			success: function(result){
-
-				let login = false;
-
-				if( result.login === true ){
-					login = true;
-				}else{
-					loginFail(); //show dialog
-				}
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.USER_LOGIN,
-					data: result
-				});
-			},
-			error: function(err){
-
-				loginFail(); //show dialog
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.USER_LOGIN,
-					data: false
-				});
-			}
-		});
+		let url = address + '/login';
+		fetch(url, {
+			method: 'POST',
+			body: data
+		})
+		.then( res => res.json())
+		.then( result => dispatcher('USER_LOGIN', result))
+		.catch( err => dispatcher('USER_LOGIN', false));
 	},
 
 	signUp( data, loginSuccess ){
-		$.ajax({
-			url: address + '/',
-			type: 'POST',
-			data: data,
-
-			success: function(result){
-
-				loginSuccess();
-
-				console.log('result', result);
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.noop,
-					data: null
-				});
-			},
-			error: function(err){
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.noop,
-					data: null
-				});
-			}
-		});
+		let url = address + '/';
+		fetch(url, {
+			metod: 'POST'
+		})
+		.then( res => res.json())
+		.then( result => {
+			loginSuccess();
+			return dispatcher('noop', null);
+		})
+		.catch( err => dispatcher('noop', null));
 	},
 
 	forgetPwd( data ) {
-		$.ajax({
-			url: address + '/pwd?email=' + data.email,
-			type: 'GET',
-
-			success: function(result){
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.noop,
-					data: null
-				});
-			},
-			error: function(err){
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.noop,
-					data: null
-				});
-			}
-		});
+		let url = address + '/pwd?email=' + data.email;
+		fetch(url)
+		.then( res => res.json())
+		.then( result => dispatcher('noop', null))
+		.catch( err => dispatcher('noop', null));
 	},
 
 	changeDisplay( page ) {
-
-		//conosle.log('page', )
-
-		AppDispatcher.handleViewAction({
-			actionType: AppConstants.CHANGE_DISPLAY,
-			data: page
-		});
+		dispatcher('CHANGE_DISPLAY', page);
 	},
 
 	loaduserList() {
-		$.ajax({
-			url: streamAaddress + '/',
-			type: 'GET',
-
-			success: function(result){
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.USER_LOAD,
-					data: result
-				});
-			},
-			error: function(err){
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.noop,
-					data: null
-				});
-			}
-		});
+		let url = streamAaddress + '/';
+		fetch(url)
+		.then( res => res.json())
+		.then( result => dispatcher('USER_LOAD', result))
+		.catch( err => dispatcher('noop', null));
 	},
 
 	follow( uid, him) {
-		$.ajax({
-			url: followAddress + '/',
-			type: 'POST',
-			data: { uid, him },
-			success: function(result){
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.USER_FOLLOW,
-					data: him
-				});
-			},
-			error: function(err){
-
-				AppDispatcher.handleViewAction({
-					actionType: AppConstants.noop,
-					data: null
-				});
-			}
-		});
+		let url = followAddress + '/';
+		fetch(url)
+		.then( res => res.json())
+		.then( result => dispatcher('USER_FOLLOW', result))
+		.catch( err => dispatcher('noop', null));
 	},
 
 	unfollow( uid, him) {
